220389eb6bb84b8a1606db40655c80b34c5f10d6
Na._.kyung <leena3823@gmail.com>
2023-08-30 05:18:57 +0900

    쿨쿨

--- Text/main.js
+++ Text/main.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
         $('.l-navbar').toggleClass('expander');
     });
 
-    // 내비게이션 링크 클릭 시 컨텐츠 보이기
+   
     $('.nav__link').on('click', function(e) {
         e.preventDefault(); // 링크의 기본 동작을 방지
         $('.nav__link').removeClass('active'); // 모든 링크에서 활성 상태 제거
@@ -125,4 +125,93 @@ $(document).ready(function() {
     $('.btn-close-modal').on('click', function() {
         $('.modal').fadeOut();
     });
+});
+
+function DropFile(dropAreaId, fileListId) {
+    let dropArea = document.getElementById(dropAreaId);
+    let fileList = document.getElementById(fileListId);
+  
+    function preventDefaults(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  
+    function highlight(e) {
+      preventDefaults(e);
+      dropArea.classList.add("highlight");
+    }
+  
+    function unhighlight(e) {
+      preventDefaults(e);
+      dropArea.classList.remove("highlight");
+    }
+  
+    function handleDrop(e) {
+      unhighlight(e);
+      let dt = e.dataTransfer;
+      let files = dt.files;
+  
+      handleFiles(files);
+  
+      const fileList = document.getElementById(fileListId);
+      if (fileList) {
+        fileList.scrollTo({ top: fileList.scrollHeight });
+      }
+    }
+  
+    function handleFiles(files) {
+      files = [...files];
+      files.forEach(previewFile);
+    }
+  
+    function previewFile(file) {
+      console.log(file);
+      fileList.appendChild(renderFile(file));
+    }
+  
+    function renderFile(file) {
+      let fileDOM = document.createElement("div");
+      fileDOM.className = "file";
+      fileDOM.innerHTML = `
+        <div class="thumbnail">
+          <img src="https://img.icons8.com/pastel-glyph/2x/image-file.png" alt="파일타입 이미지" class="image">
+        </div>
+        <div class="details">
+          <header class="header">
+            <span class="name">${file.name}</span>
+            <span class="size">${file.size}</span>
+          </header>
+          <div class="progress">
+            <div class="bar"></div>
+          </div>
+          <div class="status">
+            <span class="percent">100% done</span>
+            <span class="speed">90KB/sec</span>
+          </div>
+        </div>
+      `;
+      return fileDOM;
+    }
+  
+    dropArea.addEventListener("dragenter", highlight, false);
+    dropArea.addEventListener("dragover", highlight, false);
+    dropArea.addEventListener("dragleave", unhighlight, false);
+    dropArea.addEventListener("drop", handleDrop, false);
+  
+    return {
+      handleFiles
+    };
+  }
+  
+  const dropFile = new DropFile("drop-file", "files");
+
+//   select
+  $('.selected-language').click(function() {
+    $('.dropdown-menu').toggle();
+});
+
+$('.dropdown-menu li').click(function() {
+    var selectedLang = $(this).data('lang');
+    $('.selected-language').text(selectedLang);
+    $('.dropdown-menu').hide();
 });
