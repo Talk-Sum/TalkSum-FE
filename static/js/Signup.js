671044fb11e240858a461742117a817472f9b0ac
Na._.kyung <leena3823@gmail.com>
2023-09-19 19:13:14 +0900

    gg

--- static/js/Signup.js
+++ static/js/Signup.js
@@ -1,23 +1,19 @@
-const forms = document.querySelector(".forms"),
-      pwShowHide = document.querySelectorAll(".eye-icon"),
-      links = document.querySelectorAll(".link");
+const pwShowHide = document.querySelectorAll(".eye-icon");
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-        
-        pwFields.forEach(password => {
-            if(password.type === "password"){
-                password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
+        let pwField = eyeIcon.previousElementSibling;  // 아이콘의 바로 이전 sibling 요소를 가져옵니다.
+
+        if (pwField.type === "password") {
+            pwField.type = "text";
+            eyeIcon.classList.replace("bx-hide", "bx-show");
+        } else {
+            pwField.type = "password";
             eyeIcon.classList.replace("bx-show", "bx-hide");
-        })
-        
-    })
-})      
+        }
+    });
+});
+    
 
 links.forEach(link => {
     link.addEventListener("click", e => {
@@ -26,7 +22,25 @@ links.forEach(link => {
     })
 })
 
+function redirectToLogin() {
+    window.location.href = "Login.html";
+}
 
-        function redirectToLogin() {
-            window.location.href = "Login.html";
-        }
+function checkPasswords() {
+    // 비밀번호 입력 필드의 값 가져오기
+    var password = document.getElementById("password1").value;
+    var confirmPassword = document.getElementById("password2").value;
+
+    // 두 값이 동일한지 확인
+    if (password !== confirmPassword) {
+        alert("비밀번호가 일치하지 않습니다.");
+        return false;
+    } else {
+        alert("비밀번호가 일치합니다.");
+        return true;
+    }
+}
+
+
+
+        
