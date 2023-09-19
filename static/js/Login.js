c20b2161430c168b96a9d69dc3ef9b8dee485954
Na._.kyung <leena3823@gmail.com>
2023-09-19 16:48:54 +0900

    파이어베이스,,,하던 중

--- static/js/Login.js
+++ static/js/Login.js
@@ -26,4 +26,42 @@ links.forEach(link => {
     })
 })
 
+const googleLogin = (event) => {
+    event.preventDefault();  // Prevent the default <a> tag behavior
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        console.log("Google 로그인 성공", result);
+    }).catch((error) => {
+        console.log("Google 로그인 오류", error);
+    });
+}
+
+const facebookLogin = (event) => {
+    event.preventDefault();
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        console.log("Facebook 로그인 성공", result);
+    }).catch((error) => {
+        console.log("Facebook 로그인 오류", error);
+    });
+}
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCRGajMi8qc4fRGe9ynsVSC3XQbhmfKPlo",
+    authDomain: "talksum-a3deb.firebaseapp.com",
+    projectId: "talksum-a3deb",
+    storageBucket: "talksum-a3deb.appspot.com",
+    messagingSenderId: "964787280654",
+    appId: "1:964787280654:web:5b5d3d009f11d64e00d55f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 
